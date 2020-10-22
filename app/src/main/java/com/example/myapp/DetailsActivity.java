a46cc25f47218a43b4edf567df7afac8784fc43f
onitd <onitd@DESKTOP-RQGU8LH>
2020-10-22 13:12:58 +0300

    Creare login

---
+++ app/src/main/java/com/example/myapp/DetailsActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        ImageView imageViewTeacher = findViewById(R.id.imageViewTeacher);
+        imageViewTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(DetailsActivity.this, MainActivity.class);
+                DetailsActivity.this.startActivity(intent);
+            }
+        });
+
+        ImageView imageViewStud = findViewById(R.id.imageViewStud);
+        imageViewStud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(DetailsActivity.this, MainActivity.class);
+                DetailsActivity.this.startActivity(intent);
+            }
+        });
+
+        ImageView imageViewParent = findViewById(R.id.imageViewParent);
+        imageViewParent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(DetailsActivity.this, MainActivity.class);
+                DetailsActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.myapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 public class MainActivity extends AppCompatActivity {
+     ConstraintLayout constraintLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        constraintLayout=findViewById(R.id.container);
 
     }
 }
